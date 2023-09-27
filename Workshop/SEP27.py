bc8db0b3f8827293adb90706cfcecbf4bd5f7e3c
Joe Han <jhoon.han@outlook.com>
2023-09-27 12:41:43 +0100

    work finished

---
+++ Workshop/SEP27.py
@@ -0,0 +1,49 @@
+# print(f"a: {8}")
+# # 8
+# print(f"b: {8*2}")
+# # 16
+# print(f"c: {8**2}")
+# # 64
+# print(f"d: {8/12}")
+# # 0.666...
+# print(f"e: {8//12}")
+# # 0
+# print(f"f: {8/0}")
+# # ERROR
+
+# print("Joe", "18 Coles Lane", "07469 463056")
+# name = "Joe"
+# print("My name is", name)
+
+# width = int(input("Enter width : "))
+# height = int(input("Enter height : "))
+# area = width * height
+# print("The area is", area, "square units.")
+
+# int("7")
+# width = int(input("Enter width :"))
+# height = int(input("Enter height :"))
+# area = width * height
+# print(area)
+
+# Triangle
+# base = int(input("Base? :"))
+# height = int(input("Height? :"))
+
+# area = (base * height) * 0.5
+# print(area)
+
+# Circle
+# radius = int(input("Radius? :"))
+
+# area = (3.14 * radius) ** 2
+# print(area)
+
+# number = input("What numba? ")
+
+# number_multiplied = number + 1
+# print(number_multiplied)
+
+# # Invalide
+# number_multiplied = int(number) + 1
+# print(number_multiplied)
