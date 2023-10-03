5d78e6c96c909aaf7950dd5fe9b7497c3fcc6195
Joe Han <jhoon.han@outlook.com>
2023-10-03 10:20:55 +0100

    refactored

--- index.py
+++ index.py
@@ -1,182 +1,3 @@
-# my_dogs = "aang aaang"
-
-# print(my_dogs)
-
-# print(type(my_dogs))
-
-# print(my_dogs[0])
-
-# print(len(my_dogs))
-
-# Sames as <string>.slice();
-# print(my_dogs[2:])
-# print(my_dogs[2:7])
-
-# Strings are immutable
-# name = "please God"
-
-# numbaa = 0
-# print(numbaa)
-# numbaa = 1
-# print(numbaa)
-
-# String concat
-# print(name + 'p')
-# print(name.upper())
-# print(name.capitalize())
-# print(name.split("l"))
-
-# String Interpolation
-# print('this is a string {}'.format('aaang'))
-# print('The {2} {0} {1}'.format('fox', 'brown', 'quick'))
-# print('the {q} {b} {f}'.format(f='fox', b='brown', q='quick'))
-# result = 100/77
-# print('the result was {r:1.3f}'.format(r=result))
-# print(f'hello, his name is {name}')
-# my_list = ["1", "0", "2"]
-# print(my_list[0])
-# another_list = ["3", "4"]
-# another_list[1] = "5"
-# print(my_list + another_list)
-# new_list = my_list + another_list
-# print(new_list)
-# new_list.append("six")
-# new_list.pop(0)
-# print(new_list)
-# new_list.sort()
-# print(new_list)
-# print(type(new_list))
-
-# Dictionary (Object)
-# my_dick = {"size": "big", "length": "long", "used_by": ["minseo", "Lindsey", "Yoonjeong", "Kerry", "YeWeon"]}
-# print(my_dick["used_by"][2].upper())
-# my_dick["watned_by"] = "Yoonjeong"
-# print(my_dick["watned_by"])
-
-# Tuples
-# t = (1, 2, 3)
-# print(t[2])
-# print(t.count("a"))
-
-# Sets
-# myset = set()
-# myset.add(1)
-# print(myset)
-# myset.add(2)
-# myset.add(2)
-# print(myset)
-
-# Bool
-# print(1 == "1")
-# if 1 == 1 or not 2 == 1:
-#     print("true")
-# else:
-#     print("wrong")
-
-# my_iterable = [1, 2, 3]
-
-# for item in my_iterable:
-# print(f"fuck Yoonjeong {item} times")
-# for num in my_iterable:
-#     if num % 2 == 0:
-#         print(num)
-
-# tup_arr = [(1, 2), (3, 4)]
-# for a, b in tup_arr:
-#     print(a + b)
-
-# d = {"k1": 1, "k2": 2}
-
-# for value in d.keys():
-#     print(value)
-
-# x = 0
-# while x < 5:
-#     print(x)
-#     x += 1
-# else:
-#     print("aaang")
-
-# print("y" in "yoonjeong")
-# print(int("30"))
-# mylist = [num**2 for num in range(0, 11)]
-# mylist = [x for x in range(0, 11) if x > 5]
-# results = [x if x % 2 == 0 else "odd" for x in range(0, 11)]
-# print(results)
-
-# Array methods
-# mylist = [1, 2, 3, 4]
-# mylist.insert(0, 0)
-# print(mylist)
-
-
-# def name_function(name):
-
-#     return name
-
-
-# print(name_function("shit"))
-
-
-# def checkcheck(a, b):
-#     if a > b:
-#         print("a is bigger")
-#     elif b > a:
-#         print("b is bigger")
-#     else:
-#         print("they same")
-
-
-# checkcheck(5, 1)
-
-
-# def square(num):
-# return num**2
-
-
-# my_nums = [1, 2, 3, 4, 5]
-
-
-# for el in map(square, my_nums):
-# print(el)
-
-# print(list(map(square, my_nums)))
-
-
-# lexical_name = 1
-
-
-# def lexcal_function():
-#     print("first level")
-#     print(lexical_name)
-#     lexical_name = 2
-
-#     def lexical_function2():
-#         print("second level")
-#         print(lexical_name)
-
-#     lexical_function2()
-
-
-# lexcal_function()
-
-# test_list = [1, 2, 3, 4, 5]
-# for el in test_list:
-# print(el)
-# x = 20
-
-
-# def func():
-#     print(x)
-
-
-# func()
-
-# user_input1 = input("aanag : ")
-
-# print(user_input1)
-
-
 # Tik Tak Toe
 WON_SEQUENCE = ["OOO", "XXX"]
 
@@ -188,29 +9,16 @@ game_state = {
     "user_turn_count": 0,
 }
 
-# rows
-# row0 = [" ", " ", " "]
-# row1 = [" ", " ", " "]
-# row2 = [" ", " ", " "]
-
 rows = [
     game_state["rows"]["row0"],
     game_state["rows"]["row1"],
     game_state["rows"]["row2"],
 ]
-# this doesnt work
-
-
-# User Controller
-game_finished = False
-user_turn = True
-user_turn_count = 0
 
 
 class Helpers:
     def get_user_name(self):
-        global user_turn
-        if user_turn == True:
+        if game_state["user_turn"] == True:
             return "O"
         else:
             return "X"
@@ -244,13 +52,6 @@ class InputController:
         else:
             return None
 
-    def get_user_name(self):
-        global user_turn
-        if user_turn == True:
-            return "O"
-        else:
-            return "X"
-
 
 class GameLogic:
     def __init__(self, user_turn, row, column):
@@ -271,7 +72,7 @@ class GameLogic:
         return True
 
     def check_won(self):
-        global game_finished
+        game_finished = game_state["game_finished"]
         # Horizontal
         for row in rows:
             joined_row = "".join(row)
@@ -317,17 +118,16 @@ class GameLogic:
         print(rows[2])
 
     def increase_user_turn_count(self):
-        global user_turn_count
-        user_turn_count += 1
+        game_state["user_turn_count"] += 1
 
     def switch_user_turn(self):
-        global user_turn
-        user_turn = not user_turn
+        game_state["user_turn"] = not game_state["user_turn"]
 
 
 # Controller
-while user_turn_count < 10:
-    print(f"\nCurrent turn: {user_turn_count}")
+
+while game_state["user_turn_count"] < 10:
+    print(f"\nCurrent turn: {game_state['user_turn_count']}")
 
     input_controller = InputController("Pick a row between 0,1,2 : ")
     print(f"User {Helpers().get_user_name()}'s turn")
@@ -348,7 +148,7 @@ while user_turn_count < 10:
     if passed_first != None and passed_second != None:
         # Log result
         # loggable = log_user_input(user_turn, passed_first, passed_second)
-        game_logic = GameLogic(user_turn, passed_first, passed_second)
+        game_logic = GameLogic(game_state["user_turn"], passed_first, passed_second)
         is_loggable = game_logic.log_user_input()
 
         if is_loggable != False:
