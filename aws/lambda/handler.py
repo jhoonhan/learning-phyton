c89e1f1780593bdbd0b073d7e0d02e2d6111ceae
Joe Han <jhoon.han@outlook.com>
2023-10-24 14:26:34 +0100

    testing

--- aws/lambda/handler.py
+++ aws/lambda/handler.py
@@ -39,43 +39,6 @@ def lambda_handler(event, context):
             "body": json.dumps({"message": "Message does not exist!"}),
         }
 
-    # check if message key exist in payload
-    if "message" in msg:
-        # fetching client message
-        msg = msg["message"]
-
-        if msg.lower() == "hello":
-            # response from server
-            r_msg = "Hello from server!"
-            # posts message to connected client
-            post_message(connectionId, r_msg)
-            # return statuscode
-            return {"statusCode": 200}
-
-        elif msg.lower() == "how are you":
-            r_msg = "Server is fine! How are you?"
-            post_message(connectionId, r_msg)
-            return {"statusCode": 200}
-
-        elif msg.lower() == "good":
-            r_msg = "Great! It's nice talking to you"
-            post_message(connectionId, r_msg)
-            # closing the connection from server
-            response = gatewayapi.delete_connection(ConnectionId=connectionId)
-            return {"statusCode": 200}
-        else:
-            r_msg = "Thanks!"
-            post_message(connectionId, r_msg)
-            # closing the connection from server
-            response = gatewayapi.delete_connection(ConnectionId=connectionId)
-            return {"statusCode": 200}
-    else:
-        # handling if message does not exist
-        return {
-            "statusCode": 200,
-            "body": json.dumps({"message": "Message does not exist!"}),
-        }
-
 
 def post_message(connectionId, msg):
     gateway_resp = gatewayapi.post_to_connection(
