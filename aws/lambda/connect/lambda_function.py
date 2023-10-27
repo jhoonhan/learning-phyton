69b51162f3f7607c43783523df915143658b931d
Joe Han <jhoon.han@outlook.com>
2023-10-27 18:20:19 +0100

    don't rellay need to

--- aws/lambda/connect/lambda_function.py
+++ aws/lambda/connect/lambda_function.py
@@ -2,23 +2,23 @@ import json
 import boto3
 
 URL = "https://r9mzbnosmd.execute-api.us-east-1.amazonaws.com/dev"
-gatewayapiClient = boto3.client("apigatewaymanagementapi", endpoint_url=URL)
 dynamoDbClient = boto3.client("dynamodb")
-
-
-def post_message(connectionId, msg):
-    gateway_resp = gatewayapiClient.post_to_connection(
-        ConnectionId=connectionId, Data=json.dumps({"data": msg})
-    )
+gatewayapiClient = boto3.client("apigatewaymanagementapi", endpoint_url=URL)
 
 
 def lambda_handler(event, context):
     connectionId = event["requestContext"].get("connectionId")
     dynamoDbClient.put_item(
         TableName="ticTacToe-connections",
-        Item={"connectionId": {"S": connectionId},
+        Item={"connectionId": {"S": connectionId}},
     )
 
-    post_message(connectionId, "You are connected")
+    # post_message(connectionId, "You are connected")
 
     return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
+
+
+def post_message(connectionId, msg):
+    gateway_resp = gatewayapiClient.post_to_connection(
+        ConnectionId=connectionId, Data=json.dumps({"data": msg})
+    )
