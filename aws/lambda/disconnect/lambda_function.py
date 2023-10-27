4e8571b638af274cedfc3cde2602ff340bb2f0eb
Joe Han <jhoon.han@outlook.com>
2023-10-27 17:44:34 +0100

    delete item added

---
+++ aws/lambda/disconnect/lambda_function.py
@@ -0,0 +1,13 @@
+import json
+import boto3
+
+
+def lambda_handler(event, context):
+    client = boto3.client("dynamodb")
+    client.delete_item(
+        TableName="ticTacToe-connections",
+        Key={"connectionId": {"S": event["requestContext"].get("connectionId")}},
+    )
+
+    # TODO implement
+    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
