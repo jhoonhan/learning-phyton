b2bebacf23fea57d44bbca8f1268373e5df37b18
Joe Han <jhoon.han@outlook.com>
2023-10-28 22:24:09 +0100

    displaying rows

--- aws/lambda/gameLogic/display_status.py
+++ aws/lambda/gameLogic/display_status.py
@@ -1,8 +1,19 @@
 from post_message import post_message
 
 
-def display_status(state, gatewayapiClient, connection_id: str):
-    print(state)
+def conver_row_data(row):
+    return [
+        row["L"][0]["S"],
+        row["L"][1]["S"],
+        row["L"][2]["S"],
+    ]
 
-    msg: str = "aaaaaaaaaaa"
+
+def display_status(state, gatewayapiClient, connection_id: str):
+    rows = state["rows"]["M"]
+
+    # print(conver_row_data(rows["row0"]))
+
+    msg: str = f"{conver_row_data(rows['row0'])}\n{conver_row_data(rows['row1'])}\nconver_row_data(rows['row2'])"
+
     post_message(gatewayapiClient, connection_id, msg)
