cc52bc4063f93180d88d3b1a71210f5135cdac44
Joe Han <jhoon.han@outlook.com>
2023-10-28 21:16:22 +0100

    leggo

--- aws/lambda/gameLogic/lambdaHandler.py
+++ aws/lambda/gameLogic/lambdaHandler.py
@@ -1,22 +1,30 @@
 import json
+import boto3
+
 from GameLogic import GameLogic
-from Test import Test
 
 
 # connection URL (i.e. backend URL)
+URL = "https://r9mzbnosmd.execute-api.us-east-1.amazonaws.com/dev"
+gatewayapiClient = boto3.client("apigatewaymanagementapi", endpoint_url=URL)
+dynamoDbClient = boto3.client("dynamodb")
 
 
 def lambda_handler(event, context):
-    functionName = event["functionName"]
-    testStr = Test().tester()
     # Get host and guest connectionIds
     connectionId = event["requestContext"].get("connectionId")
-    event_body = json.loads(event["body"])
-    host_connection_id = event_body["host_connection_id"]
+
+    # Starts game
 
     # handling if message does not exist
     return {
         "statusCode": 200,
-        "body": json.dumps({"result": testStr}),
+        "body": json.dumps("workx"),
         # "body": functionName
     }
+
+
+def post_message(connectionId, msg):
+    gateway_resp = gatewayapiClient.post_to_connection(
+        ConnectionId=connectionId, Data=json.dumps({"data": msg})
+    )
